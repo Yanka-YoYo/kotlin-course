d1eb7909fa0065b45e422bc6c2f718f7c3d0d053
Yanka-YoYo <yankayankavich@gmail.com>
2024-09-13 20:41:52 +0300

    Classwork 4

--- src/main/kotlin/by/yankayoyo/kotlincourse/lesson4/Classwork4.kt
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson4/Classwork4.kt
@@ -1,1 +1,28 @@
 package by.yankayoyo.kotlincourse.lesson4
+
+val muInt: Int = 0
+val myFloat: Float = 0.0F
+val myLong: Long = 1_234_567
+val myShort: Short = 327
+val myByte: Byte = 127
+val myDouble: Double = 1.9999
+
+val isKotlinFun: Boolean =true
+
+val letter: Char = '1'
+
+val text: String = "abcd"
+
+val numbers: Array<Int> = arrayOf(1, 2, 3)
+
+val string: MutableList<String> = mutableListOf("one", "two", "three")
+
+val doubles: Set<Double> = setOf(23.3, 25.5)
+
+val keysToValues: Map<String, String> = mapOf(
+    "Key 1" to "Enything 1"
+    "Key 2" to "Enything 2"
+)
+
+val anything: Any = false
+
