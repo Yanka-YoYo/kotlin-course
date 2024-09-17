9d077e84c812ed319272a7fcf7daeae254ef8e5c
Yanka-YoYo <yankayankavich@gmail.com>
2024-09-17 19:45:41 +0300

    Homework 4

---
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson5/Classwork5.kt
@@ -0,0 +1,18 @@
+package by.yankayoyo.kotlincourse.lesson5
+
+fun main () {
+    val sum = 10 + 5
+    val diff = 10 - 5
+    val product = 10 * 5
+    val  remainder = 10 % 5
+    val isEqual = 5 == 5
+    val isNotEqual = 5 != 5
+    val isGreater = 5 > 3
+    val isLesser = 5 < 3
+    val isGreaterOrEqual = 5 >= 5
+    val isLesserOrEqual = 5 <= 3
+    val andResult = false
+    val orResult = !(5 > 3)
+
+    println(sum)
+}
