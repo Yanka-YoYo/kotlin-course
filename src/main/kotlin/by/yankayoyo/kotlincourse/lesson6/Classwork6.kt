0d948ecc85ffd40a5c1754b1c3819ad22a579af4
Yanka-YoYo <yankayankavich@gmail.com>
2024-09-20 21:14:26 +0300

    Homework 5

--- src/main/kotlin/by/yankayoyo/kotlincourse/lesson6/Classwork6.kt
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson6/Classwork6.kt
@@ -1,2 +1,47 @@
 package by.yankayoyo.kotlincourse.lesson6
 
+// intRange // ot i do vkluchitelno
+// intRangeUntil // ot i do ne vkl poslednee
+// eti 2 // vsegda ot menshego k bolshemu
+// downTo // ot bolshego k menshemu
+// charRange // lkz cbvdjkjd
+// when (vznachenie) {
+       // in (virazhenie1) -> {*cod*}
+       // in (virazenie 2) => {*cod*}
+       // ...
+      // else -> printLn ("")
+
+fun main () {
+}
+
+fun checkPass (password : String) : Boolean {
+
+    // if (password.length <= 8 ) return false
+
+    // if (checkSymbols(password)) return true
+
+    // return false
+
+    return checkSymbols(password)
+
+}
+
+fun checkSymbols (password: String) : Boolean {
+
+    return false
+}
+
+fun convertMark(mark: Int): String{
+
+    val result = when (mark){
+        in 0 .. 20 -> "F"
+        in 21 .. 40 -> "E"
+        in 41 .. 60 -> "D"
+        in 61 .. 80 -> "C"
+        in 81 .. 90 -> "B"
+        in 91 .. 100 -> "A"
+        else -> "Invalid mark"
+    }
+ return result
+
+}
---
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson6/Homework6.kt
@@ -0,0 +1,4 @@
+package by.yankayoyo.kotlincourse.lesson6
+
+class Homework6 {
+}
