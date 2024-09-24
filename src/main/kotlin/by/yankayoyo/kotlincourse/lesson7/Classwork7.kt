7b69ca792f658e2f446a58344bb66fafd7a3d4f5
Yanka-YoYo <yankayankavich@gmail.com>
2024-09-24 21:07:08 +0300

    Classwork7

--- src/main/kotlin/by/yankayoyo/kotlincourse/lesson7/Classwork7.kt
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson7/Classwork7.kt
@@ -1,2 +1,72 @@
 package by.yankayoyo.kotlincourse.lesson7
 
+fun main() {
+    for (item in 1 .. 10 step 2) {
+        println(item)
+    }
+
+    for (i in 1..10) {
+        println(i)
+    }
+
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+
+    for (i in 1 until 10) {
+        println(i)
+    }
+
+val range = 1..30
+    for (i in range) {
+        println(i)
+    }
+
+    var counter = 10
+    while (counter-- > 0) {
+        println(counter)
+    }
+
+    do {
+        println("+")
+    } while (false)
+
+//    for (i3 in 1..10 {
+  //      if (i3 == 2) continue
+    //    if (i3 == 7) break
+      //  printLn(i3)
+    //}
+
+    for (item2 in 1..10) {
+        println(item2)
+    }
+
+    for (i in 1..5) {
+       println(i*i)
+    }
+
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+
+for (i in 20 downTo 2) {
+    if (i % 2 == 0) {
+        println(i)
+    }
+}
+
+    for (i in 1..30 step 3) {
+        println(i)
+    }
+
+    var count = 1
+    var sum = 0
+    while ((count++) + count <= 10) {
+    sum = sum + count++
+    }
+    println(sum)
+}
+
+
+
+
