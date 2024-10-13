28d5318f6e80fe235bc4b423063603e3a49a1820
Yanka-YoYo <yankayankavich@gmail.com>
2024-10-13 18:57:01 +0300

    HW12

--- src/main/kotlin/by/yankayoyo/kotlincourse/lesson12/Classwork12.kt
+++ src/main/kotlin/by/yankayoyo/kotlincourse/lesson12/Classwork12.kt
@@ -109,46 +109,46 @@ fun replaceString(t: MutableList<String>, m: String) {
     println("Non string")
 }
 
-fun printMap(xSize: Int, ySize: Int) {
-    checkSize(xSize, "xSize")
-    checkSize(ySize, "ySize")
-
-    val formatterSize = " $xSize".length
-
-    val xRange = createRange(xSize)
-    val yRange = createRange(ySize)
-
-    print(" ".repeat(formatterSize))
-
-    for (i in xRange) {
-        print("%${formatterSize}s".format(i))
-    }
-    println()
-    for (i in yRange) {
-        print("%${formatterSize}s".format(i))
-        for (j in yRange) {
-            val m = i * j
-            val result = when {
-                m % 2 == 0 -> if (m % 3 == 0) "." else "*"
-                m % 5 == 0 -> if (i < 0) "-" else "+"
-                else -> "?"
-            }
-            print("%${formatterSize}s".format(result))
-        }
-        println()
-    }
-}
-
-private fun checkSize(xS: Int, yS: String) {
-    if (xS == 0) throw IllegalArgumentException("$xS не должно быть равным нулю")
-}
-
-private fun createRange(size: Int): IntProgression {
-
-   return if (size > 0) {
-        0..size
-    } else {
-        0 downTo size
-    }
-}
+//fun printMap(xSize: Int, ySize: Int) {
+//    checkSize(xSize, "xSize")
+//    checkSize(ySize, "ySize")
+//
+//    val formatterSize = " $xSize".length
+//
+//    val xRange = createRange(xSize)
+//    val yRange = createRange(ySize)
+//
+//    print(" ".repeat(formatterSize))
+//
+//    for (i in xRange) {
+//        print("%${formatterSize}s".format(i))
+//    }
+//    println()
+//    for (i in yRange) {
+//        print("%${formatterSize}s".format(i))
+//        for (j in yRange) {
+//            val m = i * j
+//            val result = when {
+//                m % 2 == 0 -> if (m % 3 == 0) "." else "*"
+//                m % 5 == 0 -> if (i < 0) "-" else "+"
+//                else -> "?"
+//            }
+//            print("%${formatterSize}s".format(result))
+//        }
+//        println()
+//    }
+//}
+//
+//private fun checkSize(xS: Int, yS: String) {
+//    if (xS == 0) throw IllegalArgumentException("$xS не должно быть равным нулю")
+//}
+//
+//private fun createRange(size: Int): IntProgression {
+//
+//   return if (size > 0) {
+//        0..size
+//    } else {
+//        0 downTo size
+//    }
+//}
 
